608501e6e746919016ff738439f477833119427b
Thomas Dawson <thomas.dawson@helsinki.fi>
2026-06-11 23:53:48 +0300

    submit exercise 1.5

--- todo-app/index.js
+++ todo-app/index.js
@@ -3,10 +3,8 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
-});
-
-app.get('/', (req, res) => {
-  res.send('OK');
 });
