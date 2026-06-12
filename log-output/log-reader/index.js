b5aa647cef7438e4c13900a9db1f2c7570bdada4
Thomas Dawson <thomas.dawson@helsinki.fi>
2026-06-13 02:19:12 +0300

    ex 1.11

--- log-output/log-reader/index.js
+++ log-output/log-reader/index.js
@@ -3,21 +3,22 @@ import path from 'path';
 import {readFile} from 'fs/promises';
 
 const logFilePath = path.join('/', 'app', 'logs', 'log');
+const pongCountFilePath = path.join('/', 'app', 'data', 'count');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-app.get('/', (req, res) => {
-    readFile(logFilePath, 'utf8')
-        .then((data) => {
-            res.send(data);
-        })
-        .catch((err) => {
-            console.error('Error reading log file:', err);
-            res.status(500).send('Error reading log file');
-        });
+app.get('/', async (req, res) => {
+    try {
+        const log = await readFile(logFilePath, 'utf8');
+        const count = await readFile(pongCountFilePath, 'utf8');
+        res.type('text/plain').send(`${log}Ping / Pongs: ${count}`);
+    } catch (err) {
+        console.error('Error reading files:', err);
+        res.status(500).send('Error reading files');
+    }
 });
--- ping-pong/index.js
+++ ping-pong/index.js
@@ -1,14 +1,36 @@
+import path from 'path';
+import {readFile, writeFile} from 'fs/promises';
 import express from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-let count = 0;
+const countFilePath = path.join('/app/data', 'count');
 
-const server = app.listen(PORT, () => {
+const initializeCount = async () => {
+    try {
+        await writeFile(countFilePath, '0', {flag: 'wx'});
+    } catch (error) {
+        if (error.code !== 'EEXIST') {
+          console.error('Error initializing count file:', error);
+        }
+    }
+};
+
+await initializeCount();
+
+const readCount = async () => {
+    const rawCount = await readFile(countFilePath, 'utf8');
+    const count = Number.parseInt(rawCount, 10);
+    return Number.isNaN(count) ? 0 : count;
+};
+
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-app.get('/pingpong', (req, res) => {
-    res.send(`pong ${count++}`);
+app.get('/pingpong', async (req, res) => {
+    const count = await readCount();
+    await writeFile(countFilePath, String(count + 1));
+    res.type('text/plain').send(`pong ${count}`);
 });
