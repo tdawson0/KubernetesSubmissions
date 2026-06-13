326c499a268b5c0633df21250ae53482c31b61b1
Thomas Dawson <thomas.dawson@helsinki.fi>
2026-06-13 23:33:51 +0300

    ex 2.1

--- log-output/log-reader/index.js
+++ log-output/log-reader/index.js
@@ -3,10 +3,12 @@ import path from 'path';
 import {readFile} from 'fs/promises';
 
 const logFilePath = path.join('/', 'app', 'logs', 'log');
-const pongCountFilePath = path.join('/', 'app', 'data', 'count');
+const PING_URL = 'http://ping-pong-svc:1234/pings';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
@@ -15,7 +17,8 @@ app.listen(PORT, () => {
 app.get('/', async (req, res) => {
     try {
         const log = await readFile(logFilePath, 'utf8');
-        const count = await readFile(pongCountFilePath, 'utf8');
+        const response = await fetch(PING_URL);
+        const { count } = await response.json();
         res.type('text/plain').send(`${log}Ping / Pongs: ${count}`);
     } catch (err) {
         console.error('Error reading files:', err);
--- ping-pong/index.js
+++ ping-pong/index.js
@@ -1,36 +1,18 @@
-import path from 'path';
-import {readFile, writeFile} from 'fs/promises';
 import express from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const countFilePath = path.join('/app/data', 'count');
-
-const initializeCount = async () => {
-    try {
-        await writeFile(countFilePath, '0', {flag: 'wx'});
-    } catch (error) {
-        if (error.code !== 'EEXIST') {
-          console.error('Error initializing count file:', error);
-        }
-    }
-};
-
-await initializeCount();
-
-const readCount = async () => {
-    const rawCount = await readFile(countFilePath, 'utf8');
-    const count = Number.parseInt(rawCount, 10);
-    return Number.isNaN(count) ? 0 : count;
-};
+let count = 0;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-app.get('/pingpong', async (req, res) => {
-    const count = await readCount();
-    await writeFile(countFilePath, String(count + 1));
-    res.type('text/plain').send(`pong ${count}`);
+app.get('/pings', (req, res) => {
+    res.json({ count });
+});
+
+app.get('/pingpong', (req, res) => {
+    res.type('text/plain').send(`pong ${count++}`);
 });
