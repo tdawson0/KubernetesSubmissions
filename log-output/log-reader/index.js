33bd3aedf084602ffbc9242dc0e449fd4b9baeed
Thomas Dawson <thomas.dawson@helsinki.fi>
2026-06-15 20:51:05 +0300

    ex 2.3 and 2.4

--- log-output/log-reader/index.js
+++ log-output/log-reader/index.js
@@ -14,7 +14,7 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-app.get('/', async (req, res) => {
+app.get('/logs', async (req, res) => {
     try {
         const log = await readFile(logFilePath, 'utf8');
         const response = await fetch(PING_URL);
